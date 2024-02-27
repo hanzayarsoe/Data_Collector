f72f656d6dce7496dcb3cb95c01a37966bb635c1
hanzayarsoe <hanzayarsoe404@gmail.com>
2024-02-27 11:23:30 +0630

    alert box close after 2 sec

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -14,16 +14,18 @@ class MyHomePage extends StatefulWidget {
 }
 
 Future<void> showAlarmDialog(
-    BuildContext context, String message, bool isError) {
-  return showDialog<void>(
+    BuildContext context, String message, bool isError) async {
+  showDialog(
     context: context,
     barrierDismissible: false, // Prevent dismissal by tapping outside
     builder: (BuildContext context) {
+      // Show dialog
       return AlertDialog(
         title: Text(
           isError ? 'Error' : 'Success',
-          style:
-              TextStyle(color: Theme.of(context).textTheme.titleLarge?.color),
+          style: TextStyle(
+            color: Theme.of(context).textTheme.titleLarge?.color,
+          ),
         ),
         content: Text(message),
         actions: [
@@ -35,6 +37,10 @@ Future<void> showAlarmDialog(
       );
     },
   );
+
+  // Close dialog after 2 seconds
+  await Future.delayed(const Duration(seconds: 2));
+  Navigator.pop(context);
 }
 
 class _MyHomePageState extends State<MyHomePage> {
@@ -116,126 +122,139 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      // Modern background color
-      appBar: AppBar(
-        leading: const Icon(Icons.folder_copy_outlined),
-        title: Text(
-          "Data Converter",
-          style: Theme.of(context).textTheme.titleLarge,
+    return KeyboardListener(
+      focusNode: FocusNode(),
+      autofocus: true,
+      onKeyEvent: (event) {
+        if (event.logicalKey == LogicalKeyboardKey.enter) {
+          handleSubmit();
+        }
+      },
+      child: Scaffold(
+        // Modern background color
+        appBar: AppBar(
+          leading: const Icon(Icons.folder_copy_outlined),
+          title: Text(
+            "Data Converter",
+            style: Theme.of(context).textTheme.titleLarge,
+          ),
+          centerTitle: false,
+          actions: [
+            (_selectedIndex == 0)
+                ? IconButton(
+                    onPressed: () {
+                      Provider.of<ThemeProvider>(context, listen: false)
+                          .toggleDarkMode();
+                    },
+                    icon: const Icon(Icons.dark_mode_outlined))
+                : IconButton(
+                    onPressed: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const MyHomePage()));
+                    },
+                    icon: const Icon(Icons.arrow_back_rounded)),
+          ], // Vibrant app bar color
         ),
-        centerTitle: false,
-        actions: [
-          (_selectedIndex == 0)
-              ? IconButton(
-                  onPressed: () {
-                    Provider.of<ThemeProvider>(context, listen: false)
-                        .toggleDarkMode();
-                  },
-                  icon: const Icon(Icons.dark_mode_outlined))
-              : IconButton(
-                  onPressed: () {
-                    Navigator.of(context).push(MaterialPageRoute(
-                        builder: (context) => const MyHomePage()));
-                  },
-                  icon: const Icon(Icons.arrow_back_rounded)),
-        ], // Vibrant app bar color
-      ),
-      body: (_selectedIndex == 0)
-          ? Center(
-              child: SingleChildScrollView(
-                child: Center(
-                  child: Padding(
-                    padding: const EdgeInsets.symmetric(
-                        horizontal: 24), // Add padding
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.center,
-                      children: [
-                        TextField(
-                          style: TextStyle(
-                              color:
-                                  Theme.of(context).textTheme.bodySmall?.color),
-                          controller: myanmar,
-                          decoration: InputDecoration(
-                            labelText: 'Myanmar',
-                            labelStyle: TextStyle(
+        body: (_selectedIndex == 0)
+            ? Center(
+                child: SingleChildScrollView(
+                  child: Center(
+                    child: Padding(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 24), // Add padding
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          TextField(
+                            style: TextStyle(
                                 color: Theme.of(context)
                                     .textTheme
-                                    .bodyLarge
+                                    .bodySmall
                                     ?.color),
-                            filled: true,
-                            fillColor:
-                                Colors.blue[50], // Lighter blue for text field
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15),
-                              borderSide: BorderSide.none,
+                            controller: myanmar,
+                            decoration: InputDecoration(
+                              labelText: 'Myanmar',
+                              labelStyle: TextStyle(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge
+                                      ?.color),
+                              filled: true,
+                              fillColor: Colors
+                                  .blue[50], // Lighter blue for text field
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide.none,
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 20),
-                        TextField(
-                          style: TextStyle(
-                              color:
-                                  Theme.of(context).textTheme.bodySmall?.color),
-                          controller: myeik,
-                          decoration: InputDecoration(
-                            labelText: 'Myeik',
-                            labelStyle: TextStyle(
+                          const SizedBox(height: 20),
+                          TextField(
+                            style: TextStyle(
                                 color: Theme.of(context)
                                     .textTheme
-                                    .bodyLarge
+                                    .bodySmall
                                     ?.color),
-                            filled: true,
-                            fillColor: Colors.blue[50],
-                            border: OutlineInputBorder(
-                              borderRadius: BorderRadius.circular(15),
-                              borderSide: BorderSide.none,
+                            controller: myeik,
+                            decoration: InputDecoration(
+                              labelText: 'Myeik',
+                              labelStyle: TextStyle(
+                                  color: Theme.of(context)
+                                      .textTheme
+                                      .bodyLarge
+                                      ?.color),
+                              filled: true,
+                              fillColor: Colors.blue[50],
+                              border: OutlineInputBorder(
+                                borderRadius: BorderRadius.circular(15),
+                                borderSide: BorderSide.none,
+                              ),
                             ),
                           ),
-                        ),
-                        const SizedBox(height: 25),
-                        ElevatedButton(
-                          onPressed: handleSubmit,
-                          style: ElevatedButton.styleFrom(
-                            backgroundColor: Colors.blue[800],
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(15),
+                          const SizedBox(height: 25),
+                          ElevatedButton(
+                            onPressed: handleSubmit,
+                            style: ElevatedButton.styleFrom(
+                              backgroundColor: Colors.blue[800],
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(15),
+                              ),
+                              padding: const EdgeInsets.symmetric(
+                                  vertical: 15, horizontal: 15),
+                              textStyle: const TextStyle(fontSize: 16),
                             ),
-                            padding: const EdgeInsets.symmetric(
-                                vertical: 15, horizontal: 15),
-                            textStyle: const TextStyle(fontSize: 16),
+                            child: const Text(
+                              'Store Data',
+                              style: TextStyle(color: Colors.white),
+                            ),
                           ),
-                          child: const Text(
-                            'Store Data',
-                            style: TextStyle(color: Colors.white),
-                          ),
-                        ),
-                      ],
+                        ],
+                      ),
                     ),
                   ),
                 ),
-              ),
-            )
-          : const DataList(),
-      bottomNavigationBar: BottomNavigationBar(
-        selectedItemColor: Theme.of(context).appBarTheme.iconTheme?.color,
-        items: const [
-          BottomNavigationBarItem(
-            icon: Icon(
-              Icons.home,
-              size: 28,
-            ),
-            label: 'Home',
-          ),
-          BottomNavigationBarItem(
+              )
+            : const DataList(),
+        bottomNavigationBar: BottomNavigationBar(
+          selectedItemColor: Theme.of(context).appBarTheme.iconTheme?.color,
+          items: const [
+            BottomNavigationBarItem(
               icon: Icon(
-                Icons.book_outlined,
+                Icons.home,
                 size: 28,
               ),
-              label: 'Data Lists'),
-        ],
-        currentIndex: _selectedIndex, // Keep track of current page
-        onTap: _onItemTapped,
+              label: 'Home',
+            ),
+            BottomNavigationBarItem(
+                icon: Icon(
+                  Icons.book_outlined,
+                  size: 28,
+                ),
+                label: 'Data Lists'),
+          ],
+          currentIndex: _selectedIndex, // Keep track of current page
+          onTap: _onItemTapped,
+        ),
       ),
     );
   }
